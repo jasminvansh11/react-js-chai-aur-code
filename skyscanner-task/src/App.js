95e10d1d611719e28375e27f119d8f714e19ef0a
Jasmin Vansh <jasminvansh06@gmail.com>
2026-01-01 06:50:58 +0000

    skyScanner Project

---
+++ skyscanner-task/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Flight Schedule</h1>
+      <input type='date'/>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
