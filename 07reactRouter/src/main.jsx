7aea3aa017e19b0e0451603c6635ed8e71d2ff3a
Jasmin Vansh <jasminvansh06@gmail.com>
2026-02-16 10:22:02 +0000

    React Router Complated!

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/Github/Github.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -40,7 +40,10 @@ const router = createBrowserRouter(
       <Route path='about' element={<About/>}></Route>
       <Route path='contact' element={<Contact/>}></Route>
       <Route path='user/:userid' element={<User/>}></Route>
-      <Route path='github' element={<Github/>}></Route>
+      <Route 
+      path='github' 
+      loader={githubInfoLoader}
+      element={<Github/>}></Route>
 
 
     </Route>
